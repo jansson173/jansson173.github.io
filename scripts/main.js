323249e701befa736e0710d0be3f7ec30fb0cabd
jansson173 <andreas.jansson1@gmail.com>
2020-10-17 23:48:26 +0200

    First commit
    Added the files

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+
+const myTitle = document.querySelector('title');
+myTitle.textContent = 'First page v.1.01';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox-night.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+
+  function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
